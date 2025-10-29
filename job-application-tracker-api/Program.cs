8c0e0b04576fe2fae8ae056628086a71f5d4d76a
Nic-Thompson-JHA <nicthompsonjha@gmail.com>
2025-10-29 16:53:03 +1300

    Update Program.cs

--- job-application-tracker-api/Program.cs
+++ job-application-tracker-api/Program.cs
@@ -19,8 +19,6 @@ builder.Services.AddCors(options =>
 });
 
 builder.Services.AddControllers();
-
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
